ed84993d0d4cbf940de303a247bbe1d1993ad2be
SadiqNahmetov <sadigfn@code.edu.az>
2022-06-20 14:43:45 +0400

    Introduction C#

---
+++ Introductin/Introductin/Program.cs
@@ -0,0 +1,82 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+   //////  1:Verilmiş n - ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapın..
+
+   //int n = 21;
+
+ 
+   //if (n%3 ==0 && n%7 ==0)
+   //{
+  //    Console.WriteLine("Dividable");
+
+   //}
+
+
+
+
+
+  // 2:n və m ədədləri verilir.Əgər n və m ədədləri hər ikisi cütdürsə n və m ədədlərinin cəmini hesablayın.
+
+  //// int n=2;
+  //// int m=8;
+
+  //// if (n % 2 == 0 && m%2==0)
+  //// {
+  ////     Console.WriteLine(n+m);
+  //// }
+  //// else
+  //// {
+  ////     Console.WriteLine("ne cut nede tekdir");
+
+  //// }
+
+
+
+   // 3: Verilmis n ve m(n<m) ededleri arasindaki tek ededlerin cemini tapin.
+
+  //int n = 2;
+  //int m = 12;
+  //int sum = 0;
+
+
+   //for (int i = n; i < m; i++)
+  //{
+  //    if (i % 2==1 && i > n)
+  //    {
+   //        sum += i;
+  //    }
+  //}
+  //Console.WriteLine(sum);
+
+
+  // 4:Verilmish arrayin icindeki tek ededlerin cemini tapin.
+
+
+  //int[] numbers = { 3, 5, 6, 13, 12, 17 };
+  //int sum = 0;
+
+  //for (int i = 0; i < numbers.Length; i++)
+  //{
+  //    if (numbers[i] % 2 == 1)
+  //    {
+  //        sum += numbers[i];
+  //    }
+  //}
+ //Console.WriteLine(sum);
+
+
+  // 5: Verilmish arrayin icindeki cut ededlerin sayini tapin.
+
+  //int[] arr = { 3, 4, 5, 6, 13, 12, 20, 22 };
+  //int count = 0;
+
+  //for (int i = 0; i < arr.Length; i++)
+  //{
+  //    if (arr[i] % 2 == 0)
+  //    {
+  //        count++;
+ //    }
+ //}
+ //Console.WriteLine(count);
